d1633507bed3d30aa3a966c7ac7e201156450250
Sahar <Sahar@DESKTOP-HA6N9T1>
2017-08-07 12:25:40 +0300

    Google Analytics

--- includes/head.php
+++ includes/head.php
@@ -40,4 +40,13 @@ if (!isset($page_image))
 <script src="/js/sweetalert.js"></script>
 <script src="/js/fine-uploader.js"></script>
 <script src="/js/sahar.js"></script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-101863642-1', 'auto');
+ga('send', 'pageview');
+</script>
 </head>
